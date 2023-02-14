e43c83b9b931b3b6d8993ab1459ae27201d64809
Destiny Boone <destaj.db@gmail.com>
2023-02-14 13:29:39 -0500

    creating first bean and launching java spring confirguration

--- src/main/java/com/sandbox/learnspringframework/App01GamingBasicJava.java
+++ src/main/java/com/sandbox/learnspringframework/App01GamingBasicJava.java
@@ -5,13 +5,16 @@ import com.sandbox.learnspringframework.game.MarioGame;
 import com.sandbox.learnspringframework.game.PacManGame;
 import com.sandbox.learnspringframework.game.SuperContraGame;
 
-public class AppGamingBasicJava {
+public class App01GamingBasicJava {
 
 	public static void main(String[] args) {
 		
+		// Object creation
 		var superContraGame = new SuperContraGame();
 		var marioGame = new MarioGame();
 		var pacGame = new PacManGame();
+		
+		// Wiring dependencies (game is a dependency of GameRunner)
 		var gameRunner = new GameRunner(pacGame);
 		gameRunner.run();
 	}
---
+++ src/main/java/com/sandbox/learnspringframework/App02HelloWorldSpring.java
@@ -0,0 +1,23 @@
+package com.sandbox.learnspringframework;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.sandbox.learnspringframework.game.GameRunner;
+import com.sandbox.learnspringframework.game.MarioGame;
+import com.sandbox.learnspringframework.game.PacManGame;
+import com.sandbox.learnspringframework.game.SuperContraGame;
+
+public class App02HelloWorldSpring {
+
+	public static void main(String[] args) {
+		// Launch a Spring Context using a configuration file (HelloWorldConfiguration.java)
+		var context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+		
+		// Configure the things we want Spring to manage 
+		// HelloWorldConfiguration - @Configuration
+		// name - @Bean
+		
+		// Retrieving Beans managed by Spring
+		System.out.println(context.getBean("name"));
+	}
+}
